488ce06780316228a9abce4779b2ccf27c3123cb
edwinkuruvila <72676609+edwinkuruvila@users.noreply.github.com>
2020-10-19 16:46:43 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="instabot.py-edwinkuruvila",
+    version="0.0.1",
+    author="Edwin Kuruvila",
+    author_email="edwinkuruvila10@gmail.com",
+    description="Instagram follow bot",
+    url="https://github.com/edwinkuruvila/Instagram-follower-bot",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
